91766f89d2996e8fdfa7508a39686d77aa097880
Koen Punt <koen@koenpunt.nl>
2013-03-28 14:51:11 +0100

    Added jquery.allDone

---
+++ jquery.alldone.js
@@ -0,0 +1,26 @@
+(function($){
+  // Wait for all elements to trigger any of the supplied events
+  $.fn.allDone = function(events, callback){
+    var $this = this
+      , len = $this.length;
+
+    function triggerCallback() {
+      callback.call( $this );
+    }
+
+    function itemLoaded(event){
+      if ( --len <= 0 ){
+        setTimeout( triggerCallback );
+        $images.unbind('.allDone', itemLoaded);
+      }
+    };
+
+    if(!len){
+      return triggerCallback();
+    }
+    
+    // append event namespace for correct unbinding
+    events = events.replace(/ /g, '.allDone ') + '.allDone';
+    $this.on(events, itemLoaded);
+  };
+}).call(this, jQuery);
